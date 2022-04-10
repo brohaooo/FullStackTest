0638cebf153e11dc811b39bd8b6f86bff0e42a42
brohaooo <1916804978@qq.com>
2022-04-10 17:07:08 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+如何启动后端：https://www.bezkoder.com/node-js-express-sequelize-mysql/
+
+cd nodejs-express-sequelize-mysql
+npm install express sequelize mysql2 cors --save
+node server.js
+注意，要先确保mysql服务启动，并提前修改数据库用户信息(改成你的用户密码）并创建好同名数据库，具体在/app/config/db.config.js
+
+url:http://localhost:8080/
+
+如何启动前端：https://www.bezkoder.com/react-hooks-crud-axios-api/
+
+cd react-crud
+npm install bootstrap
+npm install react-router-dom
+npm install axios
+npm start
+
+url:http://localhost:8081/
---
+++ nodejs-express-sequelize-mysql/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+
+//?????????????
+const db = require("./app/models");
+
+
+//二选一？？？
+//db.sequelize.sync();
+
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
+
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to brohaooo application." });
+});
+
+//????
+require("./app/routes/tutorial.routes")(app);
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
+
+
+
+
+//????
+/*
+db.sequelize.sync({ force: true }).then(() => {
+    console.log("Drop and re-sync db.");
+  });
+*/
